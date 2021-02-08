fdeedd6ad4ab5545f7c9ef4e8c24f60b70bcb378
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-02-08 13:30:52 -0300

    - Agregamos el llamado a los tests

--- test3.py
+++ test3.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
 
-class DescargaAutomatica(unittest.Testcase):
+class DescargaAutomatica(unittest.TestCase):
     def setUp(self):
         profile = Options()
         profile.set_preference("browser.download.folderList",2)
@@ -18,30 +18,18 @@ class DescargaAutomatica(unittest.Testcase):
         profile.set_preference("browser.download.dir","/tmp/")
         profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-chess-pgn,application/vnd.chess-pgn")
 
-        driver = webdriver.Firefox(options=profile)
-        wait = WebDriverWait(driver, 5)
-        driver.maximize_window()
-
-    def removeAds():
-        driver.execute_script("""
-        var element = document.querySelector(".cc-banner"); if (element) element.parentNode.removeChild(element);
-        document.querySelectorAll("iframe").forEach(function(element){ element.remove(); })
-        document.querySelectorAll(".adsbygoogle").forEach(function(element){ element.remove(); })
-        document.querySelectorAll(".adsbyir").forEach(function(element){ element.remove(); })
-        """)
-
-
-    def tearDown(self):
-        self.driver.implicity_wait(5)
-        self.driver.quit()
+        self.driver = webdriver.Firefox(options=profile)
+        wait = WebDriverWait(self.driver, 5)
+        self.driver.maximize_window()
 
     def test_descargar(self):
+        driver = self.driver
         window_main = self.driver.window_handles[0] # sin uso por ahora
         web = "https://www.chessgames.com/perl/chessopening?eco=C60"
-        self.driver.get(web)
+        driver.get(web)
         #removeAds()
 
-        games_table = self.driver.find_element_by_xpath("/html/body/p[1]/table[1]")
+        games_table = driver.find_element_by_xpath("/html/body/p[1]/table[1]")
         list_games = games_table.find_elements_by_xpath("//a[starts-with(@href, '/perl/chessgame?gid=')]")
         list_links_games = []
 
@@ -51,17 +39,34 @@ class DescargaAutomatica(unittest.Testcase):
 
 
             for link_game in list_links_games:
-                self.driver.execute_script('''window.open("","_blank");''')
+                driver.execute_script('''window.open("","_blank");''')
                 time.sleep(2)
                 window_game = driver.window_handles[1]
-                self.driver.switch_to.window(window_game)
-                self.driver.get(link_game)
+                driver.switch_to.window(window_game)
+                driver.get(link_game)
 
                 download_button = driver.find_element_by_xpath("//a[text()='download']")
-                removeAds()
+                self.removeAds()
                 download_button.click()
 
                 time.sleep(2)
-                self.driver.close()
-                self.driver.switch_to.window(window_main) # si no switcheas, luego rompe
+                driver.close()
+                driver.switch_to.window(window_main) # si no switcheas, luego rompe
                 time.sleep(2)
+
+        assert "No se descargaron las partidas" not in driver.page_source
+
+    def tearDown(self):
+        self.driver.implicitly_wait(5)
+        self.driver.quit()
+
+    def removeAds(self):
+        self.driver.execute_script("""
+        var element = document.querySelector(".cc-banner"); if (element) element.parentNode.removeChild(element);
+        document.querySelectorAll("iframe").forEach(function(element){ element.remove(); })
+        document.querySelectorAll(".adsbygoogle").forEach(function(element){ element.remove(); })
+        document.querySelectorAll(".adsbyir").forEach(function(element){ element.remove(); })
+        """)
+
+if __name__ == '__main__':
+    unittest.main()
