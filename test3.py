f8082cf5cb5ecf84f1e509313586d4ab5bc3c171
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-02-08 10:14:52 -0300

    - Automatizamos el ingeso a sitios y descargar su contenido

--- test3.py
+++ test3.py
@@ -17,14 +17,6 @@ def removeAds():
     document.querySelectorAll(".adsbyir").forEach(function(element){ element.remove(); })
     """)
 
-# Mas extensiones:
-# https://www.freeformatter.com/mime-types-list.html
-
-# con FirefoxProfile() no funcionaba la opcion neverAsk, a menos que usara alwaysAsk.force "que no me parecia prudente"
-#profile = FirefoxProfile()
-#profile.set_preference("browser.helperApps.alwaysAsk.force", False);
-#profile.set_preference("browser.download.panel.shown", False)
-
 profile = Options()
 profile.set_preference("browser.download.folderList",2)
 profile.set_preference("browser.download.manager.showWhenStarting", False)
@@ -32,8 +24,6 @@ profile.set_preference("browser.download.dir","/tmp/")
 profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-chess-pgn,application/vnd.chess-pgn")
 
 driver = webdriver.Firefox(options=profile)
-#driver = webdriver.Firefox(firefox_profile=profile)
-#driver = webdriver.Firefox()
 
 driver.maximize_window()
 wait = WebDriverWait(driver, 5)
@@ -41,18 +31,34 @@ wait = WebDriverWait(driver, 5)
 web = "https://www.chessgames.com/perl/chessopening?eco=C60"
 driver.get(web)
 
-#self.selenium.switch_to.window(window_name=window_name)
-#driver.execute_script('''window.open("http://bings.com","_blank");''')
-
 # Removemos toda publicidad que impida nuestros clicks
 removeAds()
-games_table = driver.find_element_by_xpath("/html/body/p[1]/table[1]/tbody/tr/td/table[2]")
-first_game = games_table.find_elements_by_xpath("//a[starts-with(@href, '/perl/chessgame?gid=')]")[0]
-first_game.click()
 
-removeAds()
-download_button = driver.find_element_by_xpath("//a[text()='download']")
-download_button.click()
-# /html/body/center[2]/table[1]
-# $x("//a[text()='download']/@href")
-#game = games_table.find_element_by_xpath("//a[@href='/perl/chessgame?gid=1271912']")
+games_table = driver.find_element_by_xpath("/html/body/p[1]/table[1]")
+list_games = games_table.find_elements_by_xpath("//a[starts-with(@href, '/perl/chessgame?gid=')]")
+# colección tipo lista
+list_links_games = []
+
+for game in list_games:
+    link_game = game.get_attribute('href')
+    list_links_games.append(link_game)
+    # print(game.get_attribute('href'))
+
+window_main = driver.window_handles[0] # sin uso por ahora
+
+for link_game in list_links_games:
+    driver.execute_script('''window.open("","_blank");''')
+    time.sleep(2)
+    window_game = driver.window_handles[1]
+    driver.switch_to.window(window_game)
+    driver.get(link_game)
+
+    removeAds()
+    download_button = driver.find_element_by_xpath("//a[text()='download']")
+    download_button.click()
+
+    time.sleep(2)
+    driver.close()
+    driver.switch_to.window(window_main) # si no switcheas, luego rompe
+
+    time.sleep(2)
